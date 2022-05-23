914cea1be2c3fe2f6bc979884022cd547d97628b
Houssem eddine BOULAHBAL <boulahbalhoussem@gmail.com>
2022-05-23 11:35:53 +0200

    minor update

--- icp_with_kinect.py
+++ icp_with_kinect.py
@@ -1,5 +1,11 @@
 # 1. Create the Kinect device class
 # 2. Create the capture point cloud function that returns the point cloud using the kinect
-# Hint : you can reuse the code in session_2
-# 4. Capture a point cloud. define a pose using icp.SE3_exp and transform the captured point cloud using icp.transform_pointcloud
+# 3. Create the function that transforms the point cloud using a twist vector
 # 4. Using ICP estimate the pose and compare it to the ground truth
+# 5. What are the drawbacks of the current implementation ? how can you make it better ?
+#-----------------------
+# 6. Get two seccessive acquisatation of pointclouds
+# 7. Use ICP to get the pose that transforms the 2nd pointcloud to the first
+# 8. Align the two pointcloud
+# 9. What conclusion could you make about the alignement and the algorithem what improvement could be make
+# 10. Implement the improvement
